2417c44e56b5aa4c0d9e4a511d71537c2259a1e1
kanjaq <charleskanjakm4@gmail.com>
2023-12-03 14:28:47 +0300

    Add files via upload

--- CO2_Emission.py
+++ CO2_Emission.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #from #sklearn.ensemble import RandomForestRegressor
-import joblib
+#import joblib
 from PIL import Image
 #Color
 #load trained model
